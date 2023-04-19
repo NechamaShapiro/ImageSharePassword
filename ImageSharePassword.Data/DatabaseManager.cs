b60c605b0e203b364caef524db56b8d470d31557
Nechama Shapiro <nechamashapiro1234@gmail.com>
2023-04-18 21:36:06 -0400

    Add project files.

---
+++ ImageSharePassword.Data/DatabaseManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace ImageSharePassword.Data
+{
+    public class DatabaseManager
+    {
+        private string _connectionString = "Data Source=.\\sqlexpress;Initial Catalog=ImageSharePassword;Integrated Security=true;";
+        public int UploadImage(string imageName, string imagePath, string password)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = "INSERT INTO Images VALUES (@imageName, @imagePath, @password, 0) SELECT SCOPE_IDENTITY()";
+            cmd.Parameters.AddWithValue("@imageName", imageName);
+            cmd.Parameters.AddWithValue("@imagePath", imagePath);
+            cmd.Parameters.AddWithValue("@password", password);
+            connection.Open();
+            return (int)(decimal)cmd.ExecuteScalar();
+        }
+        public Image GetImageById(int id)
+        {
+            using var conn = new SqlConnection(_connectionString);
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM  Images WHERE Id = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            conn.Open();
+            var reader = cmd.ExecuteReader();
+            Image img = new Image();
+            while (reader.Read())
+            {
+                img.Id = (int)reader["Id"];
+                img.ImageName = (string)reader["ImageName"];
+                img.ImagePath = (string)reader["ImagePath"];
+                img.Password = (string)reader["Password"];
+                img.Views = (int)reader["Views"];
+            }
+            return img;
+        }
+        //public string GetPasswordById(int id)
+        //{
+        //    using var conn = new SqlConnection(_connectionString);
+        //    using var cmd = conn.CreateCommand();
+        //    cmd.CommandText = "SELECT Password FROM  Images WHERE Id = @id";
+        //    cmd.Parameters.AddWithValue("@id", id);
+        //    conn.Open();
+        //    var reader = cmd.ExecuteReader();
+        //    var password = "";
+        //    while (reader.Read())
+        //    {
+        //        password = (string)reader["Password"];
+        //    }
+        //    return password;
+        //}
+    }
+}
---
+++ ImageSharePassword.Web/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using ImageSharePassword.Data;
+using ImageSharePassword.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace ImageSharePassword.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+            //var mgr = new DatabaseManager();
+            //return View(mgr.GetAll());
+        }
+        [HttpPost]
+        public IActionResult Upload(IFormFile image, string password)
+        {
+            var imageName = $"{Guid.NewGuid()}-{image.FileName}";
+            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", imageName);
+            using var fs = new FileStream(imagePath, FileMode.CreateNew);
+            image.CopyTo(fs);
+
+            var mgr = new DatabaseManager();
+            int id = mgr.UploadImage(imageName, imagePath, password);
+            Image img = mgr.GetImageById(id);
+            return View(img);
+        }
+        public IActionResult ViewImage(int id)
+        {
+            var mgr = new DatabaseManager();
+            return View(mgr.GetImageById(id));
+        }
+        [HttpPost]
+        public IActionResult ViewImage(int id, string password)
+        {
+            var mgr = new DatabaseManager();
+            var img = mgr.GetImageById(id);
+            if(password == img.Password)
+            {
+                HttpContext.Session.Set($"{id}", true);
+            }
+            return View(img);
+        }
+    }
+    public static class SessionExtensions
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            string value = session.GetString(key);
+
+            return value == null ? default(T) :
+                JsonSerializer.Deserialize<T>(value);
+        }
+    }
+}
